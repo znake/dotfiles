de55a2181f124ddf15725642300c1ec5fa493b5c
Jakob <squamigera@gmail.com>
2011-04-30 18:58:05 +0200

    changed the way of implementing my own snippets for snipmate via symlinking

--- vim/mySnippets/addsnippets.sh
+++ vim/mySnippets/addsnippets.sh
@@ -2,8 +2,8 @@
 
 if [ -d ~/.vim ]; then
     if [ -d ~/.vim/bundle/snipmate.vim ]; then
-        cp ~/bin/dotfiles/vim/mySnippets/*.snippets ~/bin/dotfiles/vim/bundle/snipmate.vim/snippets
-        echo "Installed snippets successfully :)"
+        ln -nfs ~/bin/dotfiles/vim/mySnippets/*.snippets ~/bin/dotfiles/vim/bundle/snipmate.vim/snippets
+        echo "Symlinked snippets successfully :)"
     else
         echo "Please install snipMate vim plugin first!"
     fi
