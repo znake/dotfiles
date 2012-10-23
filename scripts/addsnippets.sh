7000d026b6370f80e07cfb6b52f273a1c72ae7a9
Jakob Lehner <jakob.lnr@gmail.com>
2012-10-23 10:43:20 +0200

    show added snippets ins folder

--- scripts/addsnippets.sh
+++ scripts/addsnippets.sh
@@ -5,6 +5,9 @@ if [ -d ~/.vim ]; then
     if [ -d ~/.vim/bundle/snipmate.vim ]; then
         ln -nfs $FROM $TO
         echo "Symlinked snippets successfully :)"
+        echo "**********************************"
+        echo "Installed Snippets:"
+        ls /Users/jakob/Dropbox/Library/dotfiles/vim/bundle/snipmate.vim/snippets
     else
         echo "Please install snipMate vim plugin first!"
     fi
